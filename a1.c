385658bb388bd23fe9b5f2b640530b6bf23bf445
Pscly <pscly@outlook.com>
2020-11-20 09:03:28 +0800

    刚刚测试完毕float格式化输出的  小数点展位问题

--- a1.c
+++ a1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#define CAI "冬瓜"
 int main(int argc, char const *argv[])
 {
     /* code */
+    printf("常量%s", CAI);
+
 
 }
 
--- a9.c
+++ a9.c
@@ -2,6 +2,9 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+    float a=123.0321;
+    printf("%5.2f", a);
+    return 0;
 
 }
 
