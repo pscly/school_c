0ecc587de0e45784d85eddbada33a0f0f85658fe
Pscly <pscly@outlook.com>
2020-11-20 08:38:22 +0800

    a1 计算税率

--- a1.c
+++ a1.c
@@ -3,5 +3,12 @@ int main(int argc, char const *argv[])
 {
     /* code */
 
-}
+    float user_money, shui;
 
+    printf("请输入你的工资>>:");
+    scanf("%f", &user_money);
+
+    shui = user_money > 1000 ? (user_money - 1000) * 0.05 : 0;
+
+    printf("你交了%.2f的税,拿到了%.2f", shui, user_money-shui);
+}
