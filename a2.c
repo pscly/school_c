22bc33e6fe358a02383f5343c837065d7bfd00c6
Pscly <pscly@outlook.com>
2020-11-20 09:08:23 +0800

    第二个课堂作业

---
+++ a2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int now_num;
+    printf("偶数有:\n");
+    while (now_num<10){
+        now_num++;
+        if (now_num % 2 == 0){
+            printf("%d, ", now_num);
+        }
+
+
+        
+    }
+
+}
