071d3eb02f41f910a9a82f31a9e76d957ef5e4c6
Pscly <pscly@outlook.com>
2020-11-22 20:22:18 +0800

    改动a2

--- a2.c
+++ a2.c
@@ -2,6 +2,19 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+    // 测试EOF
+
+    char input_char_1, input_char_2;
+
+    printf("请输入一个字符");
+    
+    input_char_1 = getchar();
+    if (input_char_1 == EOF)
+    {
+        printf("这个是EOF");
+    }
+    printf("============程序结束============");
+
 
 }
 
