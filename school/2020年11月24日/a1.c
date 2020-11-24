eabf6a6cfca3941f488de83d2b493de8dd1348d9
Pscly <pscly@outlook.com>
2020-11-24 16:34:46 +0800

    a1 两个while 的打印* 第一个版本(自己写的)

---
+++ school/2020年11月24日/a1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int num_1, num_2;
+    num_1 = 0;
+    while (num_1 < 10)
+    {
+        /* code */
+
+        num_1++;
+
+        for (size_t i = 0; i < num_1; i++)
+        {
+            /* code */
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+
+
