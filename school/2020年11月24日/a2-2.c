b96a49761327dad6c64313cbad4b9027dc23105a
Pscly <pscly@outlook.com>
2020-11-24 17:45:14 +0800

    作业2(a2-2) 用了更好的算法(又修补一下注释)

--- school/2020年11月24日/a2-2.c
+++ school/2020年11月24日/a2-2.c
@@ -3,8 +3,8 @@ int main(int argc, char const *argv[])
 {
     /* code */
     int input_num, sum_num;
-    int new_input_num=0;
-    int now_num=0;
+    int new_input_num = 0;
+    int now_num = 0;
     sum_num = 0;
     printf("请输入一个数字:");
     scanf("%d", &input_num);
@@ -13,34 +13,28 @@ int main(int argc, char const *argv[])
         /* 奇数数 code */
         new_input_num = input_num--;
     }
-    
+
     //  4+1 3+2 2+3 1+4 0+5  // 不对，重复了
     //  4+1 3+2 + 5
 
     int x = 1;
     while (1)
-    {   
-        sum_num += input_num+x;
+    {
+        sum_num += input_num + x;
 
         input_num--;
         x++;
-        if (x>input_num)
+        if (x > input_num)
         {
             /* code */
             break;
         }
-        
     }
     if (new_input_num)
     {
         /* code */
         sum_num += new_input_num;
     }
-    
-    
+
     printf("%d", sum_num);
-    
-    
-
 }
-
