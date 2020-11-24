4e9c3edc121ee9a5962762a0e6863bc64853734e
Pscly <pscly@outlook.com>
2020-11-24 17:40:41 +0800

    作业2(a2-2) 用了更好的算法

---
+++ school/2020年11月24日/a2-2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int input_num, sum_num;
+    int new_input_num=0;
+    int now_num=0;
+    sum_num = 0;
+    printf("请输入一个数字:");
+    scanf("%d", &input_num);
+    if (input_num % 2)
+    {
+        /* 奇数数 code */
+        new_input_num = input_num--;
+    }
+    
+    //  4+1 3+2 +3 1+4 0+5
+    //  4+1 3+2 + 5
+
+    int x = 1;
+    while (1)
+    {   
+        sum_num += input_num+x;
+
+        input_num--;
+        x++;
+        if (x>input_num)
+        {
+            /* code */
+            break;
+        }
+        
+    }
+    if (new_input_num)
+    {
+        /* code */
+        sum_num += new_input_num;
+    }
+    
+    
+    printf("%d", sum_num);
+    
+    
+
+}
+
