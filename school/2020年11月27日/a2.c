5224100679dd03260bd0f7f0ffb7c5fa25d53f3b
Pscly <pscly@outlook.com>
2020-11-27 09:40:05 +0800

    'a2第一次完成'

--- school/2020年11月27日/a2.c
+++ school/2020年11月27日/a2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#define MAX_NUM 100
 int main(int argc, char const *argv[])
 {
-    /* code */
-
+    int sum_num = 0;
+    for (int i = 0; i < MAX_NUM; i++)
+    {
+        if ((i % 10) == 3)
+            continue;
+        sum_num += i;
+    }
+    printf("答案是%d", sum_num);
 }
-
