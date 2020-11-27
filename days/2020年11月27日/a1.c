1d40b422500fe0a5fb520cf7cd83e8fecd92c9ed
Pscly <pscly@outlook.com>
2020-11-27 11:27:00 +0800

    简单修理

---
+++ days/2020年11月27日/a1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a=1;
+    sizeof(a);
+    printf("----");
+    printf(sizeof(a));
+    printf("----");
+
+}
--- old/4/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: e:/学校/c/4
-# Build directory: e:/学校/c/4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
