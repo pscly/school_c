42cdb85c1125eaecd23cd40820651a48c212a4cf
Pscly <pscly@outlook.com>
2020-12-08 16:19:12 +0800

    2020-12-4

--- school/2020年12月4日/a1.c
+++ school/2020年12月4日/a1.c
@@ -4,16 +4,21 @@ int main()
 {
     float results[5] = {};
 
-    for (size_t i = 0; i < 5; i++)
+    int zt = 0;
+    for (int i = 0; i < 5; i++)
     {
-        printf("\n请输入同学%d成绩:", i);
+        if (zt == 1){
+            printf("%.1f+", results[(5-1)-i]);
+            continue;
+        }
+
+        printf("\n请输入同学%d成绩:", i+1);
         scanf("%f", &results[i]);
-        
+        if (i == 4)
+            zt = 1;
+            i = -1;
     }
-    for (size_t i = 0; i < 5; i++)
-    {
-        printf("%.1f+", results[5-1-i]);
-    }
+
     
     
 
