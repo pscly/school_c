d09f9d07258ddee77666e2a20589946117c5dc0b
Pscly <pscly@outlook.com>
2020-12-08 16:36:55 +0800

    这个是方法2

--- school/2020年12月8日/a1.c
+++ school/2020年12月8日/a1.c
@@ -5,15 +5,28 @@ int main()
     int l2[10];
     
 
-    // 方法1
-    for (int i=0; i < 10; ++i)
-    {
-        l2[10-1-i] = i+1; 
+    //  方法1
+    // for (int i=0; i < 10; ++i)
+    // {
+    //     l2[10-1-i] = i+1; 
+    // }
+
+    // for (int j=0; j <10; ++j){
+    // printf(" %d ",l2[j]);
+    // }
+
+    for (size_t i = 0; i < (10/2); i++){
+        /* code */
+        size_t i1;
+        i1 = l1[10-1-i];
+        l1[10-1-i] = l1[i];
+        l1[i] = i1;
+    }
+    
+    for (int j=0; j <10; ++j){
+    printf(" %d ",l1[j]);
     }
 
-    for (int j=0; j <10; ++j){
-    printf(" %d ",l2[j]);
-    }
 
 
 
